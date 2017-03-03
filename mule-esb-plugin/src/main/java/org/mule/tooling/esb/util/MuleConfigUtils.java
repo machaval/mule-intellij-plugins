def48cfc439d0b952469035f122375a5750dbe3c
cons_eberm <eugene.berman@mulesoft.com>
2017-03-03 08:06:06 -0800

    Improved live templates

--- mule-esb-plugin/src/main/java/org/mule/tooling/esb/util/MuleConfigUtils.java
+++ mule-esb-plugin/src/main/java/org/mule/tooling/esb/util/MuleConfigUtils.java
@@ -59,8 +59,15 @@ public class MuleConfigUtils
     public static final String CONFIG_RELATIVE_PATH = "src/main/app";
 
     public static final String MULE_LOCAL_NAME = "mule";
+    public static final String MULE_FLOW_LOCAL_NAME = "flow";
+    public static final String MULE_SUB_FLOW_LOCAL_NAME = "sub-flow";
     public static final String MUNIT_TEST_LOCAL_NAME = "test";
     public static final String MUNIT_NAMESPACE = "munit";
+
+    public static final String EXCEPTION_STRATEGY_LOCAL_NAME = "exception-strategy";
+    public static final String CHOICE_EXCEPTION_STRATEGY_LOCAL_NAME = "choice-exception-strategy";
+    public static final String ROLLBACK_EXCEPTION_STRATEGY_LOCAL_NAME = "rollback-exception-strategy";
+    public static final String CATCH_EXCEPTION_STRATEGY_LOCAL_NAME = "catch-exception-strategy";
 
     public static boolean isMuleFile(PsiFile psiFile)
     {
@@ -74,7 +81,7 @@ public class MuleConfigUtils
         }
         final XmlFile psiFile1 = (XmlFile) psiFile;
         final XmlTag rootTag = psiFile1.getRootTag();
-        return isMuleConfig(rootTag);
+        return isMuleTag(rootTag);
     }
 
     public static boolean isMUnitFile(PsiFile psiFile)
@@ -89,7 +96,7 @@ public class MuleConfigUtils
         }
         final XmlFile psiFile1 = (XmlFile) psiFile;
         final XmlTag rootTag = psiFile1.getRootTag();
-        if (rootTag == null || !isMuleConfig(rootTag))
+        if (rootTag == null || !isMuleTag(rootTag))
         {
             return false;
         }
@@ -97,9 +104,45 @@ public class MuleConfigUtils
         return munitTags.length > 0;
     }
 
-    private static boolean isMuleConfig(XmlTag rootTag)
+    public static boolean isMuleTag(XmlTag rootTag)
     {
         return rootTag.getLocalName().equalsIgnoreCase(MULE_LOCAL_NAME);
+    }
+
+    public static boolean isFlowTag(XmlTag rootTag)
+    {
+        return rootTag.getLocalName().equalsIgnoreCase(MULE_FLOW_LOCAL_NAME);
+    }
+    public static boolean isExceptionStrategyTag(XmlTag rootTag)
+    {
+        return rootTag.getLocalName().equalsIgnoreCase(EXCEPTION_STRATEGY_LOCAL_NAME) ||
+                rootTag.getLocalName().equalsIgnoreCase(CHOICE_EXCEPTION_STRATEGY_LOCAL_NAME) ||
+                rootTag.getLocalName().equalsIgnoreCase(CATCH_EXCEPTION_STRATEGY_LOCAL_NAME) ||
+                rootTag.getLocalName().equalsIgnoreCase(ROLLBACK_EXCEPTION_STRATEGY_LOCAL_NAME);
+    }
+    public static boolean isSubFlowTag(XmlTag rootTag)
+    {
+        return rootTag.getLocalName().equalsIgnoreCase(MULE_SUB_FLOW_LOCAL_NAME);
+    }
+    public static boolean isMUnitTestTag(XmlTag rootTag)
+    {
+        return rootTag.getLocalName().equalsIgnoreCase(MUNIT_TEST_LOCAL_NAME);
+    }
+    public static boolean isTopLevelTag(XmlTag tag) {
+        return isFlowTag(tag) || isSubFlowTag(tag) || isMUnitTestTag(tag) || isExceptionStrategyTag(tag);
+    }
+
+    public static boolean isInTopLevelTag(XmlTag tag) {
+        boolean inTopLevel = false;
+        XmlTag current = tag;
+
+        while (!inTopLevel && current != null) {
+            inTopLevel = MuleConfigUtils.isTopLevelTag(current);
+            if (!inTopLevel )
+                current = current.getParentTag();
+        }
+
+        return inTopLevel;
     }
 
     public static QName getQName(XmlTag xmlTag)
@@ -616,7 +659,7 @@ public class MuleConfigUtils
     public static String getMulePath(XmlTag tag)
     {
         final LinkedList<XmlTag> elements = new LinkedList<>();
-        while (!isMuleConfig(tag))
+        while (!isMuleTag(tag))
         {
             elements.push(tag);
             tag = tag.getParentTag();
