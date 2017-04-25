6fb0056342051c4e35427131737c56722fbed1e6
cons_eberm <eugene.berman@mulesoft.com>
2017-04-25 16:07:44 -0400

    Mule global library automatically added to new projects

--- mule-esb-plugin/src/main/java/org/mule/tooling/esb/wizard/MuleMavenModuleBuilder.java
+++ mule-esb-plugin/src/main/java/org/mule/tooling/esb/wizard/MuleMavenModuleBuilder.java
@@ -1,15 +1,24 @@
 package org.mule.tooling.esb.wizard;
 
-import com.intellij.facet.*;
+import com.intellij.facet.Facet;
+import com.intellij.facet.FacetManager;
+import com.intellij.facet.FacetTypeRegistry;
+import com.intellij.facet.ModifiableFacetModel;
 import com.intellij.ide.util.projectWizard.ModuleWizardStep;
 import com.intellij.ide.util.projectWizard.SourcePathsBuilder;
 import com.intellij.ide.util.projectWizard.WizardContext;
 import com.intellij.openapi.Disposable;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.options.ConfigurationException;
 import com.intellij.openapi.project.DumbAwareRunnable;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ModifiableRootModel;
+import com.intellij.openapi.roots.ModuleRootManager;
+import com.intellij.openapi.roots.ModuleRootModificationUtil;
+import com.intellij.openapi.roots.libraries.Library;
+import com.intellij.openapi.roots.libraries.LibraryTable;
+import com.intellij.openapi.roots.libraries.LibraryTablesRegistrar;
 import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.vfs.LocalFileSystem;
@@ -18,6 +27,7 @@ import org.jetbrains.annotations.Nullable;
 import org.jetbrains.idea.maven.model.MavenId;
 import org.jetbrains.idea.maven.utils.MavenUtil;
 import org.jetbrains.idea.maven.wizards.MavenModuleBuilder;
+import org.mule.tooling.esb.framework.MuleLibraryKind;
 import org.mule.tooling.esb.framework.facet.MuleFacet;
 import org.mule.tooling.esb.framework.facet.MuleFacetConfiguration;
 import org.mule.tooling.esb.framework.facet.MuleFacetType;
@@ -46,7 +56,11 @@ public class MuleMavenModuleBuilder extends MavenModuleBuilder implements Source
         final Project project = rootModel.getProject();
         final VirtualFile root = createAndGetContentEntry();
         rootModel.addContentEntry(root);
-        MavenUtil.runWhenInitialized(project, (DumbAwareRunnable) () -> new MuleMavenProjectBuilderHelper().configure(project, getProjectId(), muleVersion, root));
+        MavenUtil.runWhenInitialized(project, (DumbAwareRunnable) () -> {
+            new MuleMavenProjectBuilderHelper().configure(project, getProjectId(), muleVersion, root);
+            setMuleFramework(rootModel.getModule());
+        });
+
     }
 
     private VirtualFile createAndGetContentEntry() {
@@ -101,5 +115,29 @@ public class MuleMavenModuleBuilder extends MavenModuleBuilder implements Source
         ModifiableFacetModel model = FacetManager.getInstance(module).createModifiableModel();
         model.addFacet(facet);
         model.commit();
+    }
+
+    public void setMuleFramework(Module module) {
+        final ModifiableRootModel model = ModuleRootManager.getInstance(module).getModifiableModel();
+        MuleSdk sdk = MuleSdkManager.getInstance().findFromVersion(muleVersion);
+        if (sdk != null) {
+            String libraryName = MuleLibraryKind.MULE_LIBRARY_KIND.getKindId() + "-" + muleVersion;
+            LibraryTable table = LibraryTablesRegistrar.getInstance().getLibraryTableByLevel(LibraryTablesRegistrar.APPLICATION_LEVEL, module.getProject());
+
+            Library[] libs = table.getLibraries();
+            for (final Library lib : libs) {
+                if (lib.getName().equalsIgnoreCase(libraryName)) {
+
+                    ApplicationManager.getApplication().runWriteAction(() ->
+                    {
+                        ModuleRootModificationUtil.addDependency(module, lib);
+                        model.addLibraryEntry(lib);
+                        lib.getModifiableModel().commit();
+                        model.commit();
+                    });
+                    break;
+                }
+            }
+        }
     }
 }
