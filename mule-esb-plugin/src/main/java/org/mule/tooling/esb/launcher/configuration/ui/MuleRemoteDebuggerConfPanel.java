9b33f6f8bc0770d90a02bf71658161ca25536f76
cons_eberm <eugene.berman@mulesoft.com>
2017-08-07 10:07:03 -0700

    Remote Debug minor UI fixes

--- mule-esb-plugin/src/main/java/org/mule/tooling/esb/launcher/configuration/ui/MuleRemoteDebuggerConfPanel.java
+++ mule-esb-plugin/src/main/java/org/mule/tooling/esb/launcher/configuration/ui/MuleRemoteDebuggerConfPanel.java
@@ -4,64 +4,72 @@ package org.mule.tooling.esb.launcher.configuration.ui;
 import org.mule.tooling.esb.launcher.configuration.MuleRemoteConfiguration;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.InputMethodEvent;
-import java.awt.event.InputMethodListener;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.NumberFormatter;
+import java.text.NumberFormat;
 
 public class MuleRemoteDebuggerConfPanel {
-  private JTextField hostTextField;
-  private JTextField portTextField;
-  private JTextField mulePropertiesTextField;
-  private JPanel mainPanel;
+    private JTextField hostTextField;
+    private JFormattedTextField portTextField;
+    private JTextField mulePropertiesTextField;
+    private JPanel mainPanel;
 
-  public MuleRemoteDebuggerConfPanel() {
+    public MuleRemoteDebuggerConfPanel() {
 
-    updatePropertiesValue();
-    portTextField.addInputMethodListener(new InputMethodListener() {
-      @Override
-      public void inputMethodTextChanged(InputMethodEvent event) {
         updatePropertiesValue();
-      }
+        portTextField.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                updatePropertiesValue();
+            }
 
-      @Override
-      public void caretPositionChanged(InputMethodEvent event) {
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                updatePropertiesValue();
+            }
 
-      }
-    });
-
-    portTextField.addActionListener(new ActionListener() {
-      @Override
-      public void actionPerformed(ActionEvent e) {
-        updatePropertiesValue();
-      }
-    });
-
-  }
-
-  public void updateFromConfig(MuleRemoteConfiguration configuration) {
-    hostTextField.setText(configuration.getHost());
-    portTextField.setText(String.valueOf(configuration.getPort()));
-    updatePropertiesValue();
-  }
-
-  private void updatePropertiesValue() {
-    mulePropertiesTextField.setText("-debug -M-Dmule.debug.enable=true -M-Dmule.debug.port=" + portTextField.getText());
-  }
-
-  public JPanel getMainPanel() {
-    return mainPanel;
-  }
-
-  public String getHostText() {
-    return hostTextField.getText();
-  }
-
-  public int getPortNumber() {
-    try {
-      return Integer.parseInt(portTextField.getText());
-    } catch (Exception e) {
-      return 6666;
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                updatePropertiesValue();
+            }
+        });
     }
-  }
+
+    public void updateFromConfig(MuleRemoteConfiguration configuration) {
+        hostTextField.setText(configuration.getHost());
+        portTextField.setText(String.valueOf(configuration.getPort()));
+        updatePropertiesValue();
+    }
+
+    private void updatePropertiesValue() {
+        mulePropertiesTextField.setText("-debug -M-Dmule.debug.enable=true -M-Dmule.debug.port=" + portTextField.getText());
+    }
+
+    public JPanel getMainPanel() {
+        return mainPanel;
+    }
+
+    public String getHostText() {
+        return hostTextField.getText();
+    }
+
+    public int getPortNumber() {
+        try {
+            return Integer.parseInt(portTextField.getText());
+        } catch (Exception e) {
+            return 6666;
+        }
+    }
+
+    private void createUIComponents() {
+        NumberFormat format = NumberFormat.getInstance();
+        format.setGroupingUsed(false);
+        NumberFormatter formatter = new NumberFormatter(format);
+        formatter.setValueClass(Integer.class);
+        formatter.setMaximum(65535);
+        formatter.setAllowsInvalid(false);
+        formatter.setCommitsOnValidEdit(true);
+        portTextField = new JFormattedTextField(formatter);
+    }
 }
